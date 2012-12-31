c0de1248527dec2875da8421c4462b4eeba1d926
Ryan Riley <ryan.riley@panesofglass.org>
2012-12-31 15:50:03 -0600

    Add tests.

---
+++ test/WebApiContrib.Formatting.MsgPack.Tests/MessagePackMediaTypeFormatterTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Should;
+using WebApiContrib.Formatting.MsgPack;
+
+namespace WebApiContrib.Formatting.MsgPack.Tests
+{
+    public class Url
+    {
+        public int UrlId { get; set; }
+        public string Address { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+    }
+
+    public static class MessagePackMediaTypeFormatterTests
+    {
+        static HttpContent content;
+        static MessagePackMediaTypeFormatter formatter;
+        static Url data;
+
+        [TestFixtureSetUp]
+        public static void SetUp()
+        {
+            data = new Url
+            {
+                UrlId = 1,
+                Address = "http://webapicontrib.github.com/",
+                Title = "Web API Contrib",
+                CreatedAt = DateTime.Now,
+                CreatedBy = "Me",
+            };
+
+            formatter = new MessagePackMediaTypeFormatter();
+
+            content = new ObjectContent<Url>(data, formatter);
+        }
+
+        [TestFixtureTearDown]
+        public static void TearDown()
+        {
+            content.Dispose();
+            content = null;
+        }
+
+        [Test]
+        public static void Test_Should_Have_Headers()
+        {
+            content.Headers.ShouldNotBeNull();
+            content.Headers.ShouldNotBeEmpty();
+        }
+
+        [Test]
+        public static void Test_Content_Has_Correct_MediaType()
+        {
+            content.Headers.ContentType.ShouldNotBeNull();
+            content.Headers.ContentType.MediaType.ShouldBeSameAs("application/x-msgpack");
+        }
+
+        [Test]
+        public static void Test_Content_Should_Be_Readable_As_Url()
+        {
+            var url = content.ReadAsAsync<Url>(new[] { formatter }).Result;
+            url.ShouldBeSameAs(data);
+        }
+    }
+}
--- tools/psake/default.ps1
+++ tools/psake/default.ps1
@@ -4,12 +4,12 @@ Properties {
 	$build_artifacts_dir = "$base_dir\build"
 	$solution_name = "$base_dir\WebApiContrib.Formatting.MsgPack.sln"
 	$test_dll = "$build_artifacts_dir\WebApiContrib.Formatting.MsgPack.Tests.dll"
-	$nunit_runner = "$packages_dir\NUnit.Runners.2.6.0.12051\tools"
+	$nunit_runner = "$packages_dir\NUnit.Runners.2.6.2\tools"
 	$nunit_build_destination = "$build_artifacts_dir\tools\nunit"
 	$nunitConsole = "$nunit_build_destination\nunit-console.exe"
 }
 
-Task Default -Depends BuildWebApiContrib #, PrepareForTest, RunUnitTests
+Task Default -Depends BuildWebApiContrib, PrepareForTest, RunUnitTests
 
 Task BuildWebApiContrib -Depends Clean, Build
 
